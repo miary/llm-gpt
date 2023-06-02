8b8f47551ed1a5f608b34a7c38cd8ba06893d8ad
Miary Andria <themia@gmail.com>
2023-06-02 04:06:16 -0700

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,43 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 from constants import CHROMA_SETTINGS
 import chromadb
 
+#--------- UI Components ------------------#
+st.set_page_config(
+        page_title="CBP - LLM",
+        page_icon=":robot:",
+)
+
+st.sidebar.image("assets/CBP-logo.png")
+st.sidebar.header("CBP Assets Query")
+st.sidebar.markdown("1. Assets and models are localized.")
+st.sidebar.markdown("2. Documents are within the firewall.")
+st.sidebar.markdown("3. Queries remain within the intranet.")
+
+st.sidebar.divider()
+st.sidebar.markdown("Document types can be:")
+st.sidebar.markdown("- MS Word, Excell")
+st.sidebar.markdown("- PDF and Text documents")
+st.sidebar.markdown("- HTML documents -- from sitemap or individual links")
+st.sidebar.markdown("- Outook, ePub")
+st.sidebar.markdown("- Many more")
+
+
 def list_of_collections():
     client = chromadb.Client(CHROMA_SETTINGS)
     return (client.list_collections())
     
 def main():
-    st.title("PrivateGPT App: Document Embedding and Retrieval")
+    st.title("CTO Office - LLM Solution")
     
     # Document upload section
-    st.header("Document Upload")
+    # st.header("Document Upload")
     files = st.file_uploader("Upload document", accept_multiple_files=True)
     # collection_name = st.text_input("Collection Name") not working for some reason
     if st.button("Embed"):
         embed_documents(files, "collection_name")
     
     # Query section
-    st.header("Document Retrieval")
+    # st.header("Document Retrieval")
     collection_names = get_collection_names()
     selected_collection = st.selectbox("Select a document", collection_names)
     if selected_collection:
@@ -77,15 +98,16 @@ def retrieve_documents(query: str, collection_name: str):
     response = requests.post(endpoint, params=data)
     if response.status_code == 200:
         result = response.json()
-        st.subheader("Results")
-        st.text(result["results"])
+        #st.subheader("Results")
+        st.text_area(result["results"])
         
-        st.subheader("Documents")
+        #st.subheader("Documents")
         for doc in result["docs"]:
             st.text(doc)
     else:
         st.error("Failed to retrieve documents.")
-        st.write(response.text)
+        #st.write(response.text)
+        st.text_area(response.text)
 
 
 if __name__ == "__main__":
